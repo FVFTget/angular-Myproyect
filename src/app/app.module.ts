5f256bdd9b379f1ddaac4f655c110fbefaa48673
Veronica <99519576+FVFTget@users.noreply.github.com>
2022-05-28 12:49:04 -0300

    Se agrega el Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { PieComponent } from './componentes/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    PieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
